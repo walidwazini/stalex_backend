39c7b981c4ae75784f6b59e8f70af33679eeb29e
Walid Wazini <wazini25@gmail.com>
2022-09-21 19:02:57 +0800

    GET routes for products

--- controllers/product-ctrl.js
+++ controllers/product-ctrl.js
@@ -15,3 +15,23 @@ exports.create = asyncHandler(async (req, res) => {
   }
 
 })
+
+exports.getAllProducts = asyncHandler(async (req, res) => {
+  // const pageSize = 6
+  // const page = Number(req.query.pageNumber) || 1
+
+
+  const products = await Product.find().sort({ _id: -1 })
+
+  res.json(products)
+})
+
+exports.getSingleProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  try {
+    const product = await Product.findById(id)
+    res.status(200).json(product)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+})
